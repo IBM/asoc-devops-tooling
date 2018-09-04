8aed7e3adccccdc6ef609b2fcc1311d52503d0db
Crystal Chun <crystalchun@crystals-mbp.raleigh.ibm.com>
2018-09-04 12:49:38 -0400

    Changes: -oso flag is an optional variable, added the travis file, and the scan id is now displayed after the scan is started.

--- travis-appscan-automation/getJson.py
+++ travis-appscan-automation/getJson.py
@@ -1,10 +1,16 @@
 import json
 import sys
 
+# Loads a json file in and gets the field specified.
+# Usage: python getJson.py fileName "FieldName"
+# Limitations: Only works for JSON objects and the field name
+# is case-sensitive.
+
 def main():
     data = load_data()
     printFields(data)
 
+# Gets the contents of the JSON file specified.
 def load_data():
     data = ""
     filename = sys.argv[1]
@@ -14,6 +20,7 @@ def load_data():
         data = data[0]
     return data
 
+# Prints the specified field.
 def printFields(data):
     fields = sys.argv[2:]
     for i in fields:
